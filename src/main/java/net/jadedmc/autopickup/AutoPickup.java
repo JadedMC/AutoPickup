056037697abb043e4413a8d312ce49ed29965ceb
colby <colbykrawczyk10@gmail.com>
2023-04-25 22:07:29 -0400

    Support custom drops from other plugins when breaking blocks

--- src/main/java/net/jadedmc/autopickup/AutoPickup.java
+++ src/main/java/net/jadedmc/autopickup/AutoPickup.java
@@ -4,12 +4,19 @@ import net.jadedmc.autopickup.listeners.BlockBreakListener;
 import net.jadedmc.autopickup.listeners.EntityDeathListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * This class is the main class of the plugin.
+ * It links all parts together and registers them with the server.
+ */
 public final class AutoPickup extends JavaPlugin {
 
+    /**
+     * Runs when the server is started.
+     */
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
         getServer().getPluginManager().registerEvents(new EntityDeathListener(), this);
     }
 }
--- src/main/java/net/jadedmc/autopickup/listeners/BlockBreakListener.java
+++ src/main/java/net/jadedmc/autopickup/listeners/BlockBreakListener.java
@@ -1,7 +1,11 @@
 package net.jadedmc.autopickup.listeners;
 
+import net.jadedmc.autopickup.AutoPickup;
 import net.jadedmc.autopickup.utils.InventoryUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,27 +15,63 @@ import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * This listens to the BlockBreakEvent event, which is called every time a player breaks a block.
+ * We use this to automatically add block drops to the player's inventory.
+ */
 public class BlockBreakListener implements Listener {
+    private final AutoPickup plugin;
 
+    /**
+     * To be able to access the configuration files, we need to pass an instance of the plugin to our listener.
+     * @param plugin Instance of the plugin.
+     */
+    public BlockBreakListener(AutoPickup plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Runs when the event is called.
+     * @param event BlockBreakEvent.
+     */
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
 
+        // Ignore players in creative mode.
         if(player.getGameMode() == GameMode.CREATIVE) {
             return;
         }
 
+        // Give the player the dropped experience.
         player.giveExp(event.getExpToDrop());
         event.setExpToDrop(0);
 
-        Collection<ItemStack> drops = new ArrayList<>(event.getBlock().getDrops(player.getItemInUse()));
+        // There is no way to modify drops, so to support custom drops, checks all dropped item entities 1 tick after the event.
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            Collection<ItemStack> drops = new ArrayList<>();
 
-        Collection<ItemStack> remaining = InventoryUtils.addItems(player, drops);
+            // Loop through all nearby entities to check for item entities.
+            for(Entity entity : event.getBlock().getWorld().getNearbyEntities(event.getBlock().getLocation(), 1, 1,1)) {
 
-        for(ItemStack drop : remaining) {
-            event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), drop);
-        }
+                // Skip any that aren't items.
+                if(!(entity instanceof Item)) {
+                    continue;
+                }
 
-        event.setDropItems(false);
+                // Add the item to the dropped items collection.
+                ItemStack item = ((Item) entity).getItemStack();
+                drops.add(item);
+                entity.remove();
+            }
+
+            // Add the dropped items to the player's inventory.
+            Collection<ItemStack> remaining = InventoryUtils.addItems(player, drops);
+
+            // Respawn any items that do not fit in the player's inventory.
+            for(ItemStack drop : remaining) {
+                event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), drop);
+            }
+        }, 1);
     }
 }
