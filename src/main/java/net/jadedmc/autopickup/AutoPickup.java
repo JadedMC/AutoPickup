dfee2ba5fa0f03d4dc6cde013a2d673888e7f9de
colby <colbykrawczyk10@gmail.com>
2023-04-25 20:49:58 -0400

    Add entity drops to the auto pickup

--- src/main/java/net/jadedmc/autopickup/AutoPickup.java
+++ src/main/java/net/jadedmc/autopickup/AutoPickup.java
@@ -1,6 +1,7 @@
 package net.jadedmc.autopickup;
 
 import net.jadedmc.autopickup.listeners.BlockBreakListener;
+import net.jadedmc.autopickup.listeners.EntityDeathListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AutoPickup extends JavaPlugin {
@@ -9,5 +10,6 @@ public final class AutoPickup extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+        getServer().getPluginManager().registerEvents(new EntityDeathListener(), this);
     }
 }
---
+++ src/main/java/net/jadedmc/autopickup/listeners/EntityDeathListener.java
@@ -0,0 +1,39 @@
+package net.jadedmc.autopickup.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class EntityDeathListener implements Listener {
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        Player killer = event.getEntity().getKiller();
+
+        // Makes sure the killer is still online.
+        if(killer == null) {
+            return;
+        }
+
+        // Makes sure the entity is not a player.
+        if(event.getEntity() instanceof Player) {
+            return;
+        }
+
+        // Add dropped xp to the player.
+        killer.giveExp(event.getDroppedExp());
+        event.setDroppedExp(0);
+
+        Collection<ItemStack> drops = new ArrayList<>(event.getDrops());
+        event.getDrops().clear();
+
+        for(ItemStack drop : drops) {
+            killer.getInventory().addItem(drop);
+        }
+    }
+}
